c3457ebeb89fd32d0634ae9303b52be0cda15457
isamoher <isamoher@hotmail.com>
2024-03-06 13:22:49 +0200

    adding image to book component

--- ui/src/components/book-management/Book.svelte
+++ ui/src/components/book-management/Book.svelte
@@ -7,3 +7,9 @@
 <p>Name: {book.name}</p>
 <p>Number of pages: {book.pages}</p>
 <p>ISBN: {book.isbn}</p>
+
+{#if book.cover_url && book.cover_url !== ""}
+  <img src={book.cover_url} alt="cover of the book">
+{:else}
+  <p>Cover of the book not available!</p>
+{/if}
