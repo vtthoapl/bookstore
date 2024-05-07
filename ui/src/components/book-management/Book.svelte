2e4807944c090e579e8173558c98aa17cf60878c
irina <rakhimovairinav@gmail.com>
2024-05-07 17:10:51 +0300

    Book and BookList components

--- ui/src/components/book-management/Book.svelte
+++ ui/src/components/book-management/Book.svelte
@@ -1,7 +1,8 @@
-<script>
+<script> 
   let { book } = $props();
 </script>
 
+<!--
 <h1>Viewing a book:</h1>
 
 <p>Name: {book.name}</p>
@@ -13,3 +14,28 @@
 {:else}
   <p>Cover of the book not available!</p>
 {/if}
+-->
+
+
+<div class="flex w-full">
+  <div class="w-1/2">
+    {#if book.cover_url && book.cover_url !== ""}
+      <img src={book.cover_url} alt="cover of the book" class=" mt-16 mx-32">
+    {:else}
+      <p>Cover of the book not available!</p>
+    {/if}
+  </div>
+
+  <div class="w-1/2">
+    <div class="flex">
+      <h1 class="text-4xl font-bold border-b border-[#888888] mt-16">{book.name}</h1>
+    </div>
+    <p class="font-bold text-lg text-[#9EB1C4] my-4">{book.author}</p>
+    <p class="text-[#888888] text-sm font-bold">{book.isbn}</p>
+    <h2 class="mt-10 font-bold text-lg">Sinopsis</h2>
+    <div class="flex flex-col items-center">
+      <button class="my-28 font-bold text-black bg-[#D9D9D9] hover:bg-[#888888] focus:outline-none rounded-full text-sm px-12 py-3 text-center me-2">CLEAR BOOK</button>
+    </div>
+  </div>
+</div>
+
--- ui/src/components/book-management/BookList.svelte
+++ ui/src/components/book-management/BookList.svelte
@@ -3,26 +3,43 @@
   import { useBookStore } from '../../stores/books.svelte.js';
   import { deleteBook } from '../../http-actions/books-api';
 
-  let selectedBook = $state(null); 
+  let selectedBook = null; 
   const bookStore = useBookStore();
+
+  const selectBook = (book) => {
+  console.log("Book selected:", book);
+  selectedBook = book;
+  }
 
 </script>
 
-<h1>Books</h1>
+<div class=" bg-[#F5F5F5]">
+<h1 class="text-[#000000] pt-10 pl-10 pb-3">LIST OF BOOKS</h1>
 
 {#if bookStore.books.length === 0}
-  <p>No books added yet!</p>
+  <p class="text-[#9EB1C4] font-semibold p-5">No books added yet!</p>
 {:else}
   <ul>
     {#each bookStore.books as book}
-      <li>
-        {book.name} <button on:click={() => selectedBook = book}>View</button><button on:click={() => deleteBook(book.id)}>Delete</button>
+    <div class="flex w-full border-t-2 border-[#9EB1C4] last:border-b-2">
+      <li class="text-[#9EB1C4] font-semibold p-5 w-3/6">
+        {book.name}
       </li>
+       <button on:click={() => { console.log("Button clicked"); selectBook(book); }} type="button" class="bg-[#D9D9D9] hover:bg-[#c5c4c4] h-8 rounded text-[#888888] font-bold self-center mr-3 w-2/6">View Book</button>
+       <button on:click={() => deleteBook(book.id)} type="button" class="text-[#888888] bg-red-100 hover:bg-red-200 font-bold rounded text-sm p-2.5 text-center inline-flex items-center me-2 h-8 self-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+        </svg>
+        <span class="sr-only">Icon description</span>
+        </button>
+    </div>
     {/each}
   </ul>
 {/if}
 
 {#if selectedBook}
-  <Book book={selectedBook} />
-  <button on:click={() => selectedBook = null}>Clear</button>
+  <div class="w-3/4"><Book book={selectedBook} /></div>
+  <div class="w-1/4"><BookList/></div>
 {/if}
+
+</div>
